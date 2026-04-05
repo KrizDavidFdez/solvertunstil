957f050a670321cb01dd1bac328ac4d7d16cd6d7
Samuel_4/7 <147653194+KrizDavidFdez@users.noreply.github.com>
2026-04-04 19:23:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.listen(PORT, () => {
 
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
